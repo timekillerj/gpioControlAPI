33932f2a77af4bf3b282052610a7a25b2ad4fcc7
Jason Donahue <github@timekiller.org>
2017-11-26 15:54:39 -0500

    meh

--- config.py
+++ config.py
@@ -11,13 +11,15 @@ except OSError as e:
 
 
 def get_pin(pin):
+    logging.error("GETPIN LOOKING FOR PIN: {}".format(pin))
     for module in config:
         logging.error("GETPIN MODULE: {}".format(module))
         if module == 'main':
             continue
         for mode in ['output_pins', 'input_pins']:
             logging.error("GETPIN MODE: {}".format(mode))
-            logging.error("GETPIN MODULE,MODE DATA: {}".format(config[module][mode]))
-            if pin in config[module][mode]:
+            mylist = config[module][mode]
+            logging.error("GETPIN MODULE,MODE DATA: {}".format(mylist))
+            if pin in mylist:
                 logging.error("GETPIN FOUND PIN: {}, {}".format(module, mode))
                 return (module, mode)

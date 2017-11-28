0f0ff999f4180e9da8b1fb7217b811041dfbf71b
Jason Donahue <github@timekiller.org>
2017-11-27 22:40:28 -0500

    reformat output

--- handlers/gpiocontrol.py
+++ handlers/gpiocontrol.py
@@ -16,23 +16,18 @@ class GPIOControlsHandler(BaseHandler):
     @require_shared_secret
     @gen.coroutine
     def get(self):
-        gpio_pins = {}
-        module_16relay_pins = {}
+        all_pins = {}
         try:
             # TODO use filters for INPUT/OUTPUT
             if gpio:
-                gpio_pins['input_pins'] = gpio.read_input_pins()
-                gpio_pins['output_pins'] = gpio.read_output_pins()
+                all_pins['input_pins'] = gpio.read_input_pins()
+                all_pins['output_pins'] = gpio.read_output_pins()
             if module_16relay:
-                module_16relay_pins['input_pins'] = module_16relay.read_input_pins()
-                module_16relay_pins['output_pins'] = module_16relay.read_output_pins()
+                all_pins['input_pins'].update(module_16relay.read_input_pins())
+                all_pins['output_pins'].update(module_16relay.read_output_pins())
         except Exception as e:
             logging.error("Error reading pin states: {}".format(e))
             raise tornado.web.HTTPError(500, 'Error reading pin states: {}'.format(e))
-        all_pins = {
-            "gpio": gpio_pins,
-            "module_16relay": module_16relay_pins
-        }
         self.api_response(all_pins)
 
 
